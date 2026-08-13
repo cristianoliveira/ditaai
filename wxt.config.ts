46f9160b2a4a29425d561be61ef3ddf0c7f0d34f
cristianoliveira <contato@cristianoliveira.com.br>
2026-08-13 20:39:58 +0200

    chore: bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   manifest: {
     name: 'DitaAi',
     version: '0.3.0',
-    version_name: '0.3.0-af85b7c-1786636660',
+    version_name: '0.3.0-b57b165-1786646398',
     description: 'Turn web pages into private, local audio',
     permissions: ['storage', 'tabs', 'offscreen', 'contextMenus'],
     host_permissions: ['<all_urls>'],
