b05bd413622a316a1422b6393edfed875719e5b9
cristianoliveira <contato@cristianoliveira.com.br>
2026-08-13 06:36:56 +0200

    chore: updated plans

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   manifest: {
     name: 'DitaAi',
     version: '0.3.0',
-    version_name: '0.3.0-d261ad6-1786595736',
+    version_name: '0.3.0-70cb99d-1786595816',
     description: 'Turn web pages into private, local audio',
     permissions: ['storage', 'tabs', 'offscreen', 'contextMenus'],
     host_permissions: ['<all_urls>'],
